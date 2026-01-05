c5774e6f4a81c1009f19d0980340f9ec20717ac2
Deepraj4595 <151773148+Deepraj4595@users.noreply.github.com>
2026-01-06 04:27:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+function format(string) {
+  let res = "";
+  for (let i = 0; i < string.length; i++) {
+    if (
+      (string.charCodeAt([i]) >= 65 && string.charCodeAt([i]) <= 90) ||
+      (string.charCodeAt([i]) >= 97 && string.charCodeAt([i]) <= 122)
+    ) {
+      res += string[i];
+    }
+  }
+  return res;
+}
+
 function getFormvalue() {
     //Write your code here
-
+	let firstName = format(document.querySelector("input[name='fname']").value);
+	let lastName = format(document.querySelector("input[name='lname']").value);
+	let alertMsg = firstName + " " + lastName;
+	alert(alertMsg);
 }
+
